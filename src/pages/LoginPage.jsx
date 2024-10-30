64fcd59c682028af167577f9e0de1e929278b723
JairoHervert <158542532+JairoHervert@users.noreply.github.com>
2024-10-29 19:29:18 -0600

    refactorice el codigo de la pagina de login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -22,66 +22,65 @@ function LoginPage() {
         staticNavbar={true}
       />
 
-      <div className="login-background vh-100 vw-100 d-flex justify-content-center align-items-center">
-        <div className="login-box d-flex flex-column rounded">
+      <div className='login-background vh-100 vw-100 d-flex justify-content-center align-items-center'>
+        <div className='login-box d-flex flex-column rounded'>
           {/* Icono de cerrar en la esquina superior derecha */}
-          <div className="close-icon" onClick={handleHomeClick}>✕</div>
+          <div className='close-icon' onClick={handleHomeClick}>✕</div>
 
-          <div className="login-content d-flex">
+          <div className='login-content d-flex'>
             {/* Columna izquierda: Imagen y texto */}
-            <div className="login-left">
-              <img src={imgFormulario} alt="Login background" className="login-left-image" />
+            <div className='login-left'>
+              <img src={imgFormulario} alt='Login background' className='login-left-image' />
               <div className='login-left-overlay text-start d-flex flex-column justify-content-center pb-5'>
                 <h4 className='fw-semibold fs-2 fontMontserrat'>Bienvenido a la aventura que cambiará tu historia</h4>
                 <small className='fw-light'>Únete a una comunidad vibrante donde florece la creatividad, se forjan conexiones y cada paso te acerca a experiencias inolvidables. Sumérgete y encuentra la inspiración.</small>
 
                 {/* Crédito de la fotografía */}
-                <div className="photo-credit">
+                <div className='photo-credit'>
                   Fotografía de
-                  <p><span className="fw-bold">Nombre del Fotógrafo</span></p>
+                  <p><span className='fw-bold'>Nombre del Fotógrafo</span></p>
                 </div>
               </div>
             </div>
 
             {/* Columna derecha: Formulario */}
-            <div className="login-right d-flex flex-column justify-content-center">
-              <h3 className="fw-normal mb-3 pb-3 fontMontserrat fw-semibold">Iniciar sesión</h3>
+            <div className='login-right d-flex flex-column justify-content-center'>
+              <h3 className='fw-normal mb-3 pb-3 fontMontserrat fw-semibold'>Iniciar sesión</h3>
 
-              <form className="login-form">
-                <div className="mb-3">
-                  <label htmlFor="logInputEmail" className="form-label">Correo electrónico</label>
-                  <input type="email" className="form-control" id="logInputEmail" aria-describedby="emailHelp" />
+              <form className='login-form'>
+                <div className='mb-3'>
+                  <label htmlFor='logInputEmail' className='form-label'>Correo electrónico</label>
+                  <input type='email' className='form-control' id='logInputEmail' aria-describedby='emailHelp' />
                 </div>
 
-                <div className="mb-3">
-                  <label htmlFor="logInputPassword" className="form-label">Contraseña</label>
-                  <input type="password" className="form-control" id="logInputPassword" />
+                <div className='mb-3'>
+                  <label htmlFor='logInputPassword' className='form-label'>Contraseña</label>
+                  <input type='password' className='form-control' id='logInputPassword' />
                 </div>
 
-                <div className="pt-1 mb-4 mt-4">
-                  <button type="submit" className="btn btn-primary">Iniciar sesión</button>
+                <div className='pt-1 mb-4 mt-4'>
+                  <button type='submit' className='btn btn-primary'>Iniciar sesión</button>
                 </div>
 
-                <div className="mb-3">
-                  <small><a className="text-muted" href="#!">¿Olvidaste tu contraseña?</a></small>
+                <div className='mb-3'>
+                  <small><Link className='text-muted' to='/forgot-password'>¿Olvidaste tu contraseña?</Link></small>
                 </div>
 
                 {/* Sección de botones de redes sociales */}
-                <div className="text-center mt-4 mb-5">
+                <div className='text-center mt-4 mb-5'>
                   <p>o inicia sesión con:</p>
-                  <button type="button" className="btn btn-link btn-floating mx-1">
-                    <i className="bi bi-google"></i>
+                  <button type='button' className='btn btn-link btn-floating mx-1'>
+                    <i className='bi bi-google'></i>
                   </button>
-                  <button type="button" className="btn btn-link btn-floating mx-1">
-                    <i className="bi bi-microsoft"></i>
+                  <button type='button' className='btn btn-link btn-floating mx-1'>
+                    <i className='bi bi-microsoft'></i>
                   </button>
-                  <button type="button" className="btn btn-link btn-floating mx-1">
-                    <i className="bi bi-facebook"></i>
+                  <button type='button' className='btn btn-link btn-floating mx-1'>
+                    <i className='bi bi-facebook'></i>
                   </button>
                 </div>
 
-                <p>¿No tienes una cuenta? <Link to="/register" className="fontRosaMexicano">Regístrate aquí</Link></p>
-
+                <p>¿No tienes una cuenta? <Link to='/register' className='fontRosaMexicano'>Regístrate aquí</Link></p>
               </form>
             </div>
           </div>
