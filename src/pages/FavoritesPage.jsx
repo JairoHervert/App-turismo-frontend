6884b6f774e0190928eef9cb034ac80705fe6017
JairoHervert <158542532+JairoHervert@users.noreply.github.com>
2024-11-03 00:30:57 -0600

    rama de prueba para materialUI

---
+++ src/pages/FavoritesPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FavoritesPage() {
+  return (
+    <div>
+      <h1>Página de Favoritos en rama materialUI</h1>
+    </div>
+  )
+}
+
+export default FavoritesPage
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -8,20 +8,22 @@ import ItinerariesSavedPage from '../pages/ItinerariesSavedPage';
 import TerminosCondiciones from '../pages/TerminosCondiciones';
 import PoliticasPrivacidad from '../pages/PoliticasPrivacidad';
 import ConfirmacionRegistro from '../pages/ConfirmacionRegistro';
+import FavoritesPage from '../pages/FavoritesPage';
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/usuario-deseados" element={<UsuarioDeseados />} />
-        <Route path="/itinerariesSaved" element={<ItinerariesSavedPage/>} />
-        <Route path="/terminos-condiciones" element={<TerminosCondiciones />} />
-        <Route path="/politica-privacidad" element={<PoliticasPrivacidad />} />
-        <Route path="/confirmacion-registro" element={<ConfirmacionRegistro />} />
-        <Route path="/placepage" element={<PlacePage />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/usuario-deseados' element={<UsuarioDeseados />} />
+        <Route path='/itinerariesSaved' element={<ItinerariesSavedPage/>} />
+        <Route path='/terminos-condiciones' element={<TerminosCondiciones />} />
+        <Route path='/politica-privacidad' element={<PoliticasPrivacidad />} />
+        <Route path='/confirmacion-registro' element={<ConfirmacionRegistro />} />
+        <Route path='/placepage' element={<PlacePage />} />
+        <Route path='/favorites-page' element={<FavoritesPage />} />
       </Routes>
     </Router>
   );
