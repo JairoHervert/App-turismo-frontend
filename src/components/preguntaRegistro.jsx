e3c497b3055bb7ffe7f1da4552010a94dbb57c43
Cesar H <ctorresg2100@alumno.ipn.mx>
2024-11-02 21:31:21 -0600

    seccion final del home para usuario registrado

--- src/components/preguntaRegistro.jsx
+++ src/components/preguntaRegistro.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import angelImage from '../img/HomePage/angel.jpeg';
-import Preferencias from './Preferencias';
 import '../css/HomePage.css';
 
-function preguntaRegistro() {
+
+function PreguntaRegistro() {
+    const [isLoggedIn, setIsLoggedIn] = useState(true); // Simula el estado de registro del usuario
+
     return (
         <div className="w-100 pr-0">
             <div className="container-fluid">
@@ -11,16 +13,32 @@ function preguntaRegistro() {
                     <div className="col-12 col-lg-10 p-3 pr-1" >
                         <div className="row">
                             <div className="col-12 col-md-7 d-flex flex-column justify-content-center pr-2" >
-                                <h2 className="pregunta">¿Ya te encuentras registrado?</h2>
+                                <h2 className="pregunta">
+                                    {isLoggedIn ? '¡Bienvenido!' : '¿Ya te encuentras registrado?'}
+                                </h2>
                                 <div className="pr-pitch">
-                                    <p>
-                                        ¡Regístrate ahora y lleva la organización de tus viajes al siguiente nivel! Nuestra app te permite
-                                        crear un itinerario personalizado de manera fácil y rápida, ayudándote a disfrutar de cada destino al máximo.
-                                        Con una cuenta, tendrás acceso a herramientas exclusivas para que planear sea tan emocionante como el viaje mismo.
-                                    </p>
-                                    <p>
-                                        No dejes que la falta de planificación te haga perder momentos únicos. <strong>¡Regístrate ahora y comienza a crear el itinerario de tus sueños!</strong>
-                                    </p>
+                                    {isLoggedIn ? (
+                                        <>
+                                            <p>
+                                                ¡Disfruta de todas las funcionalidades de nuestra app y lleva la organización de tus viajes al siguiente nivel! Con nuestra app, puedes crear itinerarios personalizados y recibir recomendaciones basadas en tus intereses!
+                                            </p>
+
+                                            <p>
+                                                Aprovecha todas las herramientas que hemos desarrollado para que planear tu viaje sea tan emocionante como vivirlo. ¡Explora, organiza y disfruta al máximo cada momento de tu aventura!
+                                            </p>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <p>
+                                                ¡Regístrate ahora y lleva la organización de tus viajes al siguiente nivel! Nuestra app te permite
+                                                crear un itinerario personalizado de manera fácil y rápida, ayudándote a disfrutar de cada destino al máximo.
+                                                Con una cuenta, tendrás acceso a herramientas exclusivas para que planear sea tan emocionante como el viaje mismo.
+                                            </p>
+                                            <p>
+                                                No dejes que la falta de planificación te haga perder momentos únicos. <strong>¡Regístrate ahora y comienza a crear el itinerario de tus sueños!</strong>
+                                            </p>
+                                        </>
+                                    )}
                                 </div>
                             </div>
                             <div className="col-12 col-md-5 d-flex align-items-center justify-content-center">
@@ -38,4 +56,4 @@ function preguntaRegistro() {
     );
 }
 
-export default preguntaRegistro;
+export default PreguntaRegistro;
