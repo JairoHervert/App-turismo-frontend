7de41402edd6c642507cf7a890df8d9c9eadff11
AYaelDP <128731060+AYaelDP@users.noreply.github.com>
2024-11-09 23:13:45 -0600

    Se hicieron las correcciones necesarias a la pantalla de Itinerarios guardados

--- src/pages/ItinerariesSavedPage.jsx
+++ src/pages/ItinerariesSavedPage.jsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
 import Navbar from '../components/NavBar';
 import Footer from '../components/Footer';
-import ItineraryCard from '../components/CardItinerarios';
-import SaveIcon from '../img/ItinerariesPage/icons/itineraries-guardar.png'
 import '../css/ItinerariesSavedPage.css';
+import ThemeMaterialUI from '../components/ThemeMaterialUI';
+import { ThemeProvider } from '@mui/material/styles';
+import { Container, Stack, Grid, TextField, Box, InputAdornment } from '@mui/material';
+import { Search as SearchIcon, Bookmark as BookmarkIcon} from '@mui/icons-material';
+import ItemItinerarios from '../components/CardItinerarios';
 
+
+import Imagen1 from '../img/ItinerariesPage/places/itineraries-Xochimilco.jpg';
+import Imagen2 from '../img/ItinerariesPage/places/itineraries-Grutas.jpg';
+import Imagen3 from '../img/ItinerariesPage/places/itineraries-Acuario.jpg';
 
 function ItinirariesSavePage() {
-    const navigate = useNavigate();
-
-    const handleHomeClick = () => {
-        navigate('/');
-    };
-
-    const itineraries = [
-        { id: 1, title: "Xochimilco", date: "26/07/2019", details: "Restaurante 'Fernando', florería 'Daniela', trajineras 'Xochimilco'", image: require("../img/ItinerariesPage/places/itineraries-Xochimilco.jpg")},
-        { id: 2, title: "Grutas Tolantongo", date: "26/07/2019", details: "Hotel 'Estrella', restaurante 'El señor', gruta 'Tolantongo'", image: require("../img/ItinerariesPage/places/itineraries-Grutas.jpg")},
-        { id: 3, title: "Acuario Inbursa", date: "26/07/2019", details: "Museo 'Soumaya', museo 'Jumex', cafetería 'Hello Kitty', plaza 'Antara', acuario 'Inbursa'", image: require("../img/ItinerariesPage/places/itineraries-Acuario.jpg")},
-    ];
 
     return(
-        <div>
+        <ThemeProvider theme={ThemeMaterialUI}>
             <Navbar
                 showingresa={false}
                 showRegistrate={false}
@@ -30,15 +25,53 @@ function ItinirariesSavePage() {
                 staticNavbar={false}
             />
 
-            <div className="itineraries-container">
-                <h1 className="page-title"><img src={SaveIcon} alt="save Icon" style={{ width: '40px', height: '40px' }} /> Itinerarios guardados</h1>
-                {itineraries.map(itinerary => (
-                    <ItineraryCard key={itinerary.id} itinerary={itinerary} />
-                ))}
-            </div>
+            <Container maxWidth='lg' className='my-4'>
+                <Stack direction={{ xs: 'column', sm: 'row' }} spacing={1} alignItems='center' className='mb-4' justifyContent={{ sm: 'space-between' }}>
+                    <Stack direction='row' spacing={1} alignItems='center' className='mb-2'>
+                        <BookmarkIcon color='primary' fontSize='inhert' className='fav_pag-icono-corazon' /> {/* Ahora usa el color primario del tema */}
+                        <h1 className='page-title'>Itinerarios guardados</h1>
+                    </Stack>
 
-            <Footer showIncorporaLugar={false} />
-        </div>
+                    <TextField 
+                        label="Buscar en itinerarios guardados" 
+                        variant="outlined" 
+                        size="small" 
+                        sx={{ maxWidth: 250 }}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchIcon />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                </Stack>
+
+                <ItemItinerarios
+                    imagen={Imagen1}
+                    titulo="Xochimilco"
+                    detalles="Restaurante 'Fernando', florería 'Daniela', trajineras 'Xochimilco'"
+                    fecha="26/07/2019"
+                />
+
+                <ItemItinerarios
+                    imagen={Imagen2}
+                    titulo="Grutas de Tolantongo"
+                    detalles="Hotel 'Estrella', restaurante 'El señor', gruta 'Tolantongo'"
+                    fecha="26/07/2019"
+                />
+
+                <ItemItinerarios
+                    imagen={Imagen3}
+                    titulo="Acuario Inbursa"
+                    detalles="Museo 'Soumaya', museo 'Jumex', cafetería 'Hello Kitty', plaza 'Antara', acuario 'Inbursa'"
+                    fecha="26/07/2019"
+                />               
+
+            </Container>
+
+            <Footer showIncorporaLugar={true} />
+        </ThemeProvider>
     );
     
 }
