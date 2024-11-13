78e079174991eb8fcb3506feee987c0f0e4e5332
AYaelDP <128731060+AYaelDP@users.noreply.github.com>
2024-11-12 23:16:26 -0600

    Se realizaron los cambios solicitados del css

--- src/components/CardItinerarios.jsx
+++ src/components/CardItinerarios.jsx
@@ -1,22 +1,32 @@
-import { Card, CardMedia, CardActionArea, CardActions, CardContent, Typography, Button, IconButton, Box, Grid } from '@mui/material';
+import React, { useState } from 'react';
+import { Card, CardMedia, CardActionArea, CardActions, CardContent, Typography, Button, IconButton, Box, Grid, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material';
 import { DeleteOutlineOutlined as DeleteIcon, DescriptionOutlined as DescriptionIcon } from '@mui/icons-material';
-import '../css/ItinerariesCard.css'
+import '../css/ItinerariesCard.css';
+import InfoDialog from '../components/InfoItineraries';
 
 function ItemItinerarios({ imagen, titulo, detalles, fecha }) {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
-        <Grid item xs={12} sm={6} md={4} lg={3} marginY={3} sx={{display: 'flex', justifyContent: 'center' }}>
+        <Grid item xs={12} sm={6} md={4} lg={3} marginY={3} sx={{ display: 'flex', justifyContent: 'center' }}>
             <Card className='it-res-card' sx={{ width: '85%', display: 'flex', flexDirection: 'row', boxShadow: 6 }}>
                 {/* Columna de la imagen */}
-
-
-                <CardActionArea sx={{width: 150, display: 'flex'}}>
+                <CardActionArea sx={{ width: 150, display: 'flex' }}>
                     <CardMedia
-                        component='img' 
-                        sx={{ height: '100%', objectFit: 'cover', width: 150}}
+                        component='img'
+                        sx={{ height: '100%', objectFit: 'cover', width: 150 }}
                         image={imagen}
                         alt='Lugar 1'
                     />
-                    </CardActionArea>
+                </CardActionArea>
 
                 {/* Columna de la información */}
                 <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '16px' }}>
@@ -28,15 +38,14 @@ function ItemItinerarios({ imagen, titulo, detalles, fecha }) {
                             Lugares: {detalles}
                         </Typography>
 
-                        <Box sx={{ display: 'flex', padding: '15px'}}>
-                            <Button variant="outlined" size='small'>
-                                    Más información
+                        <Box sx={{ display: 'flex', padding: '15px' }}>
+                            <Button variant="outlined" size='small' onClick={handleClickOpen}>
+                                Más información
                             </Button>
                         </Box>
                     </CardContent>
                 </Box>
 
-                {/* Columna de las acciones */}
                 {/* Columna de las acciones */}
                 <Box sx={{ minWidth: '170px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', padding: '8px', gap: '12px' }}>
                     <Typography variant='body2' color='text.secondary' component='div'>
@@ -54,12 +63,15 @@ function ItemItinerarios({ imagen, titulo, detalles, fecha }) {
                         </IconButton>
                     </Box>
                 </Box>
-
             </Card>
+
+            <InfoDialog open={open} onClose={handleClose} titulo={titulo} fecha={fecha} detalles={detalles} />
+
         </Grid>
     );
 }
 
 export default ItemItinerarios;
+
 
 
