2e3c6e8b4e812258bb6572c956d0ed4af5f60e5d
Cesar H <ctorresg2100@alumno.ipn.mx>
2024-11-30 01:35:49 -0600

    correcciones que me dio pao y el gayro

--- src/components/Alcaldias/CardAlcaldiasTotales.jsx
+++ src/components/Alcaldias/CardAlcaldiasTotales.jsx
@@ -4,7 +4,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import ButtonsMod from './ButtonsMod';
+import ButtonsMod from '../ButtonsMod';
 import { useNavigate } from 'react-router-dom';
 
 export default function CardAlcaldiasTotales({ tituloAlcaldia, descripcion, imagen }) {
