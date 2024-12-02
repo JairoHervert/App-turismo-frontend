3e921efd31159c1d605eefc636d42ec51f8365d1
John16Mark <john-16mark@hotmail.com>
2024-12-01 20:52:31 -0600

    changed navbar preferencias

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -143,7 +143,7 @@ function Navbar({ showingresa, showRegistrate, transparentNavbar, lightLink, sta
                         <li><Link className='dropdown-item' to='/recuperar-contrasena'>Recuperar Contraseña</Link></li>
                         <li><Link className='dropdown-item' to='/ingresar-nueva-contrasena'>Ingresar Nueva Contraseña</Link></li>
                         <li><Link className='dropdown-item' to='/all-places'>Todos los lugares</Link></li>
-                        <li><button className="dropdown-item" data-bs-toggle="modal" data-bs-target="#exampleModalToggle">Preferencias</button></li>
+                        <li><Link className='dropdown-item' to='/preferencias'>Preferencias</Link></li>
                         <li><button className="dropdown-item" data-bs-toggle="modal" data-bs-target="#exampleModal">Detalles</button></li>
                         <li><hr className="dropdown-divider" /></li>
                         <li><Link className="dropdown-item" to="/logout" onClick={handleLogout}>Cerrar Sesión</Link></li>
