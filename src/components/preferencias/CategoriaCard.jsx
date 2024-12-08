4c34812b57ae10b767ffb5865857f7933b4a8061
Cesar H <ctorresg2100@alumno.ipn.mx>
2024-12-07 19:12:46 -0600

    creación del componente alerta y modificación de preferencias

--- src/components/preferencias/CategoriaCard.jsx
+++ src/components/preferencias/CategoriaCard.jsx
@@ -1,48 +1,48 @@
-import React from "react";
+import React from 'react';
 import {
   Card,
   CardActionArea,
   CardMedia,
   Typography,
   Box,
-} from "@mui/material";
-import Grid from "@mui/material/Grid2";
+} from '@mui/material';
+import Grid from '@mui/material/Grid2';
 
 function CategoriaCard({ categoria, onSelect, isSelected }) {
   return (
-    <Grid size={{ xs: 12, sm: 6, md: 3.3, lg: 2.4 }}>
+    <Grid size={{ xs: 12, sm: 6, md: 3, lg: 2.4 }}>
       <Card
         sx={{
-          position: "relative",
+          position: 'relative',
           boxShadow: 3,
           mb: 2,
-          height: { xs: "auto", lg: "200px" },
-          width: { xs: "100%", lg: "200px" },
-          border: isSelected ? '2.5px solid RGB(225, 48, 167)' : 'none',
+          height: { xs: 'auto', lg: '200px' },
+          width: { xs: '100%', lg: '2' },
+          outline: isSelected ? '2.5px solid RGB(225, 48, 167)' : 'none',
         }}
       >
         <CardActionArea onClick={() => onSelect(categoria)}>
           <CardMedia
-            component="img"
-            height="200"
+            component='img'
+            height='200'
             image={categoria.imagen}
             alt={categoria.nombre}
           />
           <Box
             sx={{
-              position: "absolute",
+              position: 'absolute',
               top: 0,
               left: 0,
-              width: "100%",
-              height: "100%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              color: "white",
-              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              color: 'white',
+              backgroundColor: 'rgba(0, 0, 0, 0.5)',
             }}
           >
-            <Typography variant="h5" component="div">
+            <Typography variant='h5' sx={{textAlign: 'center'}}>
               {categoria.nombre}
             </Typography>
           </Box>
--- src/components/preferencias/CategoriasPref.js
+++ src/components/preferencias/CategoriasPref.js
@@ -1,243 +1,142 @@
-import img1 from "../../img/pref_user/categoryDeporte.webp";
-import img2 from "../../img/pref_user/categoryComidaRapida.webp";
-import img3 from "../../img/pref_user/categoryCafeteria.webp";
-import img4 from "../../img/pref_user/categoryRestaurante.webp";
-import img5 from "../../img/pref_user/categoryBar.webp";
-import img6 from "../../img/pref_user/categoryArte.webp";
-import img7 from "../../img/pref_user/categoryHistoria.webp";
-import img8 from "../../img/pref_user/categoryMuseo.webp";
-import img9 from "../../img/pref_user/categoryEducativo.webp";
-import img10 from "../../img/pref_user/categoryCompras.webp";
-import img11 from "../../img/pref_user/categoryParque.webp";
-import img12 from "../../img/pref_user/categoryJuegosRecreativosAlAireLibre.webp";
-import img13 from "../../img/pref_user/categoryJuegosRecreativosBajoTecho.webp";
-import img14 from "../../img/pref_user/categoryZoologico.webp";
-import img15 from "../../img/pref_user/categoryReligion.webp";
+import img01Arcades from "../../img/pref_user/categoryArcade.webp";
+import img02ArenasDeLuchas from "../../img/pref_user/categoryArenas.webp";
+import img03ArteYCultura from "../../img/pref_user/categoryArte.webp";
+import img04Bares from "../../img/pref_user/categoryBar.webp";
+import img05Bibliotecas from "../../img/pref_user/categoryBiblio.webp";
+import img06Boleras from "../../img/pref_user/categoryBolera.webp";
+import img07Buffetes from "../../img/pref_user/categoryBuffet.webp";
+import img08Cafeterias from "../../img/pref_user/categoryCafeteria.webp";
+import img09Clubes from "../../img/pref_user/categoryClub.webp";
+import img10ComidaRapidaYBotanas from "../../img/pref_user/categoryComidaRapida.webp";
+import img11ComprasYSouvenirs from "../../img/pref_user/categoryCompras.webp";
+import img12Karaokes from "../../img/pref_user/categoryKaraoke.webp";
+import img13LugaresDeAventura from "../../img/pref_user/categoryDeporte.webp";
+import img14LugaresHistoricos from "../../img/pref_user/categoryHistoria.webp";
+import img15LugaresReligiosos from "../../img/pref_user/categoryReligion.webp";
+import img16Marisquerias from "../../img/pref_user/categoryMarisqueria.webp";
+import img17Museos from "../../img/pref_user/categoryMuseo.webp";
+import img18Parques from "../../img/pref_user/categoryParque.webp";
+import img19ParquesDeDiversiones from "../../img/pref_user/categoryPD.webp";
+import img20ParquesDeSkate from "../../img/pref_user/categorySkate.webp";
+import img21PatinajeSobreHielo from "../../img/pref_user/categoryPatinajeHielo.webp";
+import img22Restaurantes from "../../img/pref_user/categoryRestaurante.webp";
+import img23RestaurantesAfricanos from "../../img/pref_user/categoryRestauranteAfricano.webp";
 
 const categorias = [
   {
     id: 1,
-    nombre: 'Deportes',
-    imagen: img1,
-    subcategorias: [
-      { id: 11, nombre: 'Arena' },
-      { id: 12, nombre: 'Campo atlético' },
-      { id: 13, nombre: 'Campo de golf' },
-      { id: 14, nombre: 'Pista de patinaje sobre hielo' },
-      { id: 15, nombre: 'Parque infantil' },
-      { id: 16, nombre: 'Ubicación de actividad deportiva' },
-      { id: 17, nombre: 'Club deportivo' },
-      { id: 18, nombre: 'Piscina' },
-    ],
+    nombre: 'Arcades',
+    imagen: img01Arcades,
   },
   {
     id: 2,
-    nombre: 'Comida Rápida',
-    imagen: img2,
-    subcategorias: [
-      { id: 21, nombre: 'Tienda de bagels' },
-      { id: 22, nombre: 'Tienda de açaí' },
-      { id: 23, nombre: 'Panadería' },
-      { id: 24, nombre: 'Restaurante de comida rápida' },
-      { id: 25, nombre: 'Heladería' },
-    ],
+    nombre: 'Arenas de luchas',
+    imagen: img02ArenasDeLuchas,
   },
   {
     id: 3,
-    nombre: 'Cafetería',
-    imagen: img3,
-    subcategorias: [
-      { id: 31, nombre: 'Café' },
-      { id: 32, nombre: 'Cafetería' },
-      { id: 33, nombre: 'Café de gatos' },
-      { id: 34, nombre: 'Tienda de chocolates' },
-      { id: 35, nombre: 'Cafetería' },
-      { id: 36, nombre: 'Confitería' },
-      { id: 37, nombre: 'Restaurante de postres' },
-      { id: 38, nombre: 'Tienda de postres' },
-      { id: 39, nombre: 'Comedor' },
-      { id: 40, nombre: 'Café de perros' },
-      { id: 41, nombre: 'Tienda de donas' },
-      { id: 42, nombre: 'Restaurante de alta cocina' },
-      { id: 43, nombre: 'Patio de comidas' },
-      { id: 44, nombre: 'Tienda de jugos' },
-      { id: 45, nombre: 'Comida para llevar' },
-      { id: 46, nombre: 'Tienda de sándwiches' },
-      { id: 47, nombre: 'Parrilla' },
-      { id: 48, nombre: 'Casa de té' },
-    ],
+    nombre: 'Arte y cultura',
+    imagen: img03ArteYCultura,
   },
   {
     id: 4,
-    nombre: 'Restaurante',
-    imagen: img4,
-    subcategorias: [
-      { id: 51, nombre: 'Restaurante afgano' },
-      { id: 52, nombre: 'Restaurante africano' },
-      { id: 53, nombre: 'Restaurante americano' },
-      { id: 54, nombre: 'Restaurante asiático' },
-      { id: 55, nombre: 'Restaurante de barbacoa' },
-      { id: 56, nombre: 'Restaurante brasileño' },
-      { id: 57, nombre: 'Restaurante de desayuno' },
-      { id: 58, nombre: 'Restaurante de brunch' },
-      { id: 59, nombre: 'Restaurante buffet' },
-      { id: 60, nombre: 'Restaurante chino' },
-      { id: 61, nombre: 'Restaurante francés' },
-      { id: 62, nombre: 'Restaurante griego' },
-      { id: 63, nombre: 'Restaurante de hamburguesas' },
-      { id: 64, nombre: 'Restaurante indio' },
-      { id: 65, nombre: 'Restaurante indonesio' },
-      { id: 66, nombre: 'Restaurante italiano' },
-      { id: 67, nombre: 'Restaurante japonés' },
-      { id: 68, nombre: 'Restaurante coreano' },
-      { id: 69, nombre: 'Restaurante libanés' },
-      { id: 70, nombre: 'Restaurante mediterráneo' },
-      { id: 71, nombre: 'Restaurante mexicano' },
-      { id: 72, nombre: 'Restaurante de Oriente Medio' },
-      { id: 73, nombre: 'Pizzería' },
-      { id: 74, nombre: 'Restaurante de ramen' },
-      { id: 75, nombre: 'Restaurante' },
-      { id: 76, nombre: 'Restaurante de mariscos' },
-      { id: 77, nombre: 'Restaurante español' },
-      { id: 78, nombre: 'Restaurante de sushi' },
-      { id: 79, nombre: 'Restaurante tailandés' },
-      { id: 80, nombre: 'Restaurante turco' },
-      { id: 81, nombre: 'Restaurante vegano' },
-      { id: 82, nombre: 'Restaurante vegetariano' },
-      { id: 83, nombre: 'Restaurante vietnamita' },
-    ],
+    nombre: 'Bares',
+    imagen: img04Bares,
   },
   {
     id: 5,
-    nombre: 'Bar',
-    imagen: img5,
-    subcategorias: [
-      { id: 91, nombre: 'Bar de vinos' },
-      { id: 92, nombre: 'Bar' },
-      { id: 93, nombre: 'Club nocturno' },
-      { id: 94, nombre: 'Karaoke' },
-      { id: 95, nombre: 'Salón de baile' },
-      { id: 96, nombre: 'Club de comedia' },
-      { id: 97, nombre: 'Bar y parrilla' },
-    ],
+    nombre: 'Bibliotecas',
+    imagen: img05Bibliotecas,
   },
   {
     id: 6,
-    nombre: 'Arte',
-    imagen: img6,
-    subcategorias: [
-      { id: 101, nombre: 'Galería de arte' },
-      { id: 102, nombre: 'Estudio de arte' },
-      { id: 103, nombre: 'Auditorio' },
-      { id: 104, nombre: 'Hito cultural' },
-      { id: 105, nombre: 'Teatro de artes escénicas' },
-      { id: 106, nombre: 'Escultura' },
-      { id: 107, nombre: 'Servicio de arte corporal' },
-    ],
+    nombre: 'Boleras',
+    imagen: img06Boleras,
   },
   {
     id: 7,
-    nombre: 'Historia',
-    imagen: img7,
-    subcategorias: [
-      { id: 108, nombre: 'Lugar histórico' },
-      { id: 109, nombre: 'Monumento' },
-      { id: 110, nombre: 'Hito cultural' },
-    ],
+    nombre: 'Buffetes',
+    imagen: img07Buffetes,
   },
   {
     id: 8,
-    nombre: 'Museos',
-    imagen: img8,
-    subcategorias: [
-      { id: 111, nombre: 'Museo' },
-    ],
+    nombre: 'Cafeterías',
+    imagen: img08Cafeterias,
   },
   {
     id: 9,
-    nombre: 'Educativos',
-    imagen: img9,
-    subcategorias: [
-      { id: 112, nombre: 'Biblioteca' },
-      { id: 113, nombre: 'Planetario' },
-      { id: 114, nombre: 'Acuario' },
-    ],
+    nombre: 'Clubes',
+    imagen: img09Clubes,
   },
   {
     id: 10,
-    nombre: 'Compras',
-    imagen: img10,
-    subcategorias: [
-      { id: 115, nombre: 'Tienda' },
-      { id: 116, nombre: 'Tienda de zapatos' },
-      { id: 117, nombre: 'Centro comercial' },
-      { id: 118, nombre: 'Mercado' },
-      { id: 119, nombre: 'Librería' },
-      { id: 120, nombre: 'Tienda de regalos' },
-    ],
+    nombre: 'Comida rápida y botanas',
+    imagen: img10ComidaRapidaYBotanas,
   },
   {
     id: 11,
-    nombre: 'Parques',
-    imagen: img11,
-    subcategorias: [
-      { id: 121, nombre: 'Parque' },
-      { id: 122, nombre: 'Parque estatal' },
-      { id: 123, nombre: 'Parque nacional' },
-      { id: 124, nombre: 'Jardín botánico' },
-      { id: 125, nombre: 'Jardín' },
-      { id: 126, nombre: 'Parque de vida silvestre' },
-      { id: 127, nombre: 'Refugio de vida silvestre' },
-      { id: 128, nombre: 'Parque para perros' },
-      { id: 129, nombre: 'Área de picnic' },
-      { id: 130, nombre: 'Área de barbacoa' },
-      { id: 131, nombre: 'Marina' },
-      { id: 132, nombre: 'Campamento infantil' },
-      { id: 133, nombre: 'Área de senderismo' },
-    ],
+    nombre: 'Compras y souvenirs',
+    imagen: img11ComprasYSouvenirs,
   },
   {
     id: 12,
-    nombre: 'Juegos recreativos al aire libre',
-    imagen: img12,
-    subcategorias: [
-      { id: 134, nombre: 'Parque de atracciones' },
-      { id: 135, nombre: 'Centro de atracciones' },
-      { id: 136, nombre: 'Parque acuático' },
-      { id: 137, nombre: 'Montaña rusa' },
-      { id: 138, nombre: 'Rueda de la fortuna' },
-      { id: 139, nombre: 'Parque de patinetas' },
-      { id: 140, nombre: 'Área de todoterreno' },
-      { id: 141, nombre: 'Centro de deportes de aventura' },
-      { id: 142, nombre: 'Parque de ciclismo' },
-    ],
+    nombre: 'Karaokes',
+    imagen: img12Karaokes,
   },
   {
     id: 13,
-    nombre: 'Juegos recreativos bajo techo',
-    imagen: img13,
-    subcategorias: [
-      { id: 143, nombre: 'Arcade de videojuegos' },
-      { id: 144, nombre: 'Bolera' },
-      { id: 145, nombre: 'Área de senderismo' },
-    ],
+    nombre: 'Lugares de aventura',
+    imagen: img13LugaresDeAventura,
   },
   {
     id: 14,
-    nombre: 'Zoológicos',
-    imagen: img14,
-    subcategorias: [
-      { id: 146, nombre: 'Zoológico' },
-    ],
+    nombre: 'Lugares históricos',
+    imagen: img14LugaresHistoricos,
   },
   {
     id: 15,
-    nombre: 'Religión',
-    imagen: img15,
-    subcategorias: [
-      { id: 147, nombre: 'Lugar de culto' },
-      { id: 148, nombre: 'Iglesia' },
-    ],
+    nombre: 'Lugares religiosos',
+    imagen: img15LugaresReligiosos,
+  },
+  {
+    id: 16,
+    nombre: 'Marisquerias',
+    imagen: img16Marisquerias,
+  },
+  {
+    id: 17,
+    nombre: 'Museos',
+    imagen: img17Museos,
+  },
+  {
+    id: 18,
+    nombre: 'Parques',
+    imagen: img18Parques,
+  },
+  {
+    id: 19,
+    nombre: 'Parques de diversiones',
+    imagen: img19ParquesDeDiversiones,
+  },
+  {
+    id: 20,
+    nombre: 'Parques de skate',
+    imagen: img20ParquesDeSkate,
+  },
+  {
+    id: 21,
+    nombre: 'Patinaje sobre hielo',
+    imagen: img21PatinajeSobreHielo,
+  },
+  {
+    id: 22,
+    nombre: 'Restaurantes',
+    imagen: img22Restaurantes,
+  },
+  {
+    id: 23,
+    nombre: 'Restaurantes africanos',
+    imagen: img23RestaurantesAfricanos,
   },
 ];
 
