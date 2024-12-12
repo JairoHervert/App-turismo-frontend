0d8af84e2d0690d04de0c49cdc7601795630dbf5
AYaelDP <128731060+AYaelDP@users.noreply.github.com>
2024-12-12 16:07:59 -0600

    Se agrego el dashboard en las pantallas donde faltaban

--- src/components/dashboard/ratedplaces.jsx
+++ src/components/dashboard/ratedplaces.jsx
@@ -11,6 +11,7 @@ function RatedPlacesTable({ places }) {
         sx={{
             p: 2,
             height: '100%',
+            width: '100%',
             display: 'flex',
             flexDirection: 'column',
             justifyContent: 'space-between'
