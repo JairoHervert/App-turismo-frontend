7170062a09c8e1d39f435b7f038e847396f20f84
John16Mark <john-16mark@hotmail.com>
2024-12-18 03:24:59 -0600

    Branding Aztlán Turismo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer({ showIncorporaLugar }) {
         <section>
           <div className='row'>
             <div className='col-md-3 col-lg-3 col-xl-3 mx-auto mt-3'>
-              <h6>Itinerarios Canasta Básica</h6>
+              <h6>Aztlán Turismo</h6>
               <p>
                 Descubre y organiza tus experiencias en la Ciudad de México, desde lugares icónicos hasta rincones menos conocidos.
               </p>
@@ -17,7 +17,7 @@ function Footer({ showIncorporaLugar }) {
             <hr className='w-100 clearfix d-md-none' />
 
             <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 explore-links'>
-              <h6>Canasta Básica</h6>
+              <h6>Más de nosotros</h6>
                             
               {/* Mostrar el enlace de incorporar lugar si showIncorporaLugar es verdadero */}
               {showIncorporaLugar && (
@@ -25,7 +25,7 @@ function Footer({ showIncorporaLugar }) {
               )}
 
               <p><Link to='/terminos-condiciones'>Terminos y Condiciones</Link></p>
-              <p><Link to='/'>Sobre nosotros</Link></p>
+              {/*<p><Link to='/'>Sobre nosotros</Link></p>*/}
             </div>
 
             <hr className='w-100 clearfix d-md-none' />
@@ -33,7 +33,7 @@ function Footer({ showIncorporaLugar }) {
             <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mt-3 contact-info'>
               <h6>Contacto</h6>
               <p><i className='bi bi-house-fill'></i> Ciudad de México, México</p>
-              <p><i className='bi bi-envelope-fill'></i> contacto@canastabasica.com</p>
+              <p><i className='bi bi-envelope-fill'></i> contacto@aztlanturismo.com</p>
               <p><i className='bi bi-telephone-fill'></i> +52 55 5555 5555</p>
             </div>
 
@@ -63,7 +63,7 @@ function Footer({ showIncorporaLugar }) {
       </div>
 
       <div className='copyright'>
-        © {new Date().getFullYear()} Itinerarios CDMX
+        © {new Date().getFullYear()} Aztlán Turismo
       </div>
     </footer>
   );
